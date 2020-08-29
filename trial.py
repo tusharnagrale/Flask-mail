ed3397c5b2bbf733a4dc9d92474e68b7f4a7c2e3
Tushar Nagrale <tushar.nagrale12@gmail.com>
2020-08-29 20:05:51 +0530

    Add files via upload
    this repository is for demo application for send mail

---
+++ trial.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+mail= Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'tushar.nagrale12@gmail.com'
+app.config['MAIL_PASSWORD'] = 'tushar12'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def index():
+   msg = Message('Hello', sender = 'tushar.nagrale12@gmail.com', recipients = ['tn@diycam.co.in'])
+   msg.body = "Hello Flask message sent from Flask-Mail"
+   mail.send(msg)
+   return "Sent"
+
+if __name__ == '__main__':
+   app.run(debug = True)
